6a9a10f0bfdf63699933a6b496f351f7b7323a7c
Christian Raske <git@christian-raske.de>
2019-12-12 14:19:21 +0100

    advent of code day 11 part 2

--- 11-space-police/robot/robot.go
+++ 11-space-police/robot/robot.go
@@ -2,40 +2,41 @@ package robot
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/chr-ras/advent-of-code-2019/11-space-police/intcode"
 	g "github.com/chr-ras/advent-of-code-2019/util/geometry"
 	queue "github.com/enriquebris/goconcurrentqueue"
 )
 
+// RunPaintRobot simulates the painting robot.
 func RunPaintRobot(program []int64) int {
 	memoryChannel := make(chan []int64)
 	inputQueue := queue.NewFIFO()
 	outputQueue := queue.NewFIFO()
 
-	navigationChannel := make(chan map[g.Point]color)
+	panelChannel := make(chan map[g.Point]color)
 
 	go intcode.ExecuteProgram(program, memoryChannel, inputQueue, outputQueue, 1024)
-	go navigateRobot(inputQueue, outputQueue, navigationChannel)
+	go navigateRobot(inputQueue, outputQueue, panelChannel)
 
 	<-memoryChannel
 
 	outputQueue.Enqueue(int64(99))
 
-	panels := <-navigationChannel
+	panels := <-panelChannel
 
-	for panelPoint := range panels {
-		fmt.Println(panelPoint)
-	}
+	prettyPrint(panels)
 
 	return len(panels)
 }
 
-func navigateRobot(inputQueue, outputQueue queue.Queue, navigationChannel chan map[g.Point]color) {
+func navigateRobot(inputQueue, outputQueue queue.Queue, panelChannel chan map[g.Point]color) {
 	paintedPlates := make(map[g.Point]color)
 
 	directionVector := g.Vector{X: 0, Y: -1}
 	currentPoint := g.Point{X: 0, Y: 0}
+	paintedPlates[currentPoint] = white
 
 	for {
 		currentColor := paintedPlates[currentPoint]
@@ -44,7 +45,7 @@ func navigateRobot(inputQueue, outputQueue queue.Queue, navigationChannel chan m
 		colorToPaintElement, _ := outputQueue.DequeueOrWaitForNextElement()
 		colorToPaint := colorToPaintElement.(int64)
 		if colorToPaint == 99 {
-			navigationChannel <- paintedPlates
+			panelChannel <- paintedPlates
 			return
 		} else if colorToPaint == 0 {
 			paintedPlates[currentPoint] = black
@@ -61,6 +62,55 @@ func navigateRobot(inputQueue, outputQueue queue.Queue, navigationChannel chan m
 		}
 
 		currentPoint = currentPoint.AsVector().Add(directionVector).AsPoint()
+	}
+}
+
+func prettyPrint(plates map[g.Point]color) {
+	minX, minY, maxX, maxY := math.MaxInt32, math.MaxInt32, math.MinInt32, math.MinInt32
+
+	for plate := range plates {
+		if plate.X < minX {
+			minX = plate.X
+		}
+
+		if plate.X > maxX {
+			maxX = plate.X
+		}
+
+		if plate.Y < minY {
+			minY = plate.Y
+		}
+
+		if plate.Y > maxY {
+			maxY = plate.Y
+		}
+	}
+
+	output := make([][]string, maxY-minY+1)
+	for i := range output {
+		output[i] = make([]string, maxX-minX+1)
+		for j := range output[i] {
+			output[i][j] = " "
+		}
+	}
+
+	xOffset := 0 - minX
+	yOffset := 0 - minY
+
+	for plate, color := range plates {
+		if color == white {
+			output[plate.Y+yOffset][plate.X+xOffset] = "░"
+		} else {
+			output[plate.Y+yOffset][plate.X+xOffset] = " "
+		}
+	}
+
+	for row := 0; row < len(output); row++ {
+		for col := 0; col < len(output[0]); col++ {
+			fmt.Print(output[row][col])
+		}
+
+		fmt.Println()
 	}
 }
 
