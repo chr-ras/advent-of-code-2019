7719facd73c00123c80f05fa187666658492b162
Christian Raske <git@christian-raske.de>
2019-12-19 20:20:04 +0100

    advent of code 2019 day 19 part 2

--- 19-tractor-beam/puzzle/puzzle.go
+++ 19-tractor-beam/puzzle/puzzle.go
@@ -10,7 +10,12 @@ import (
 func main() {
 	program := aoc.ReadIntcode("./drone_system_program.txt")
 
-	affectedCells := tractorbeam.ScanArea(program, 1000)
+	affectedCells := tractorbeam.ScanArea(program, 50)
 
 	fmt.Printf("Number of cells affected by tractor beam: %d\n", affectedCells)
+
+	squareSize := 100
+	closestSquareVector := tractorbeam.FindClosestSquare(program, squareSize)
+
+	fmt.Printf("Closest square in tractor beam of size %d at %v, puzzle result: %d\n", squareSize, closestSquareVector, closestSquareVector.X*10000+closestSquareVector.Y)
 }
--- 19-tractor-beam/tractorbeam/tractorbeam.go
+++ 19-tractor-beam/tractorbeam/tractorbeam.go
@@ -3,6 +3,8 @@ package tractorbeam
 import (
 	"fmt"
 
+	"github.com/chr-ras/advent-of-code-2019/util/geometry"
+
 	"github.com/chr-ras/advent-of-code-2019/util/intcode"
 	q "github.com/enriquebris/goconcurrentqueue"
 )
@@ -13,30 +15,64 @@ func ScanArea(droneSystemProgram []int64, gridSize int) int {
 
 	for y := 0; y < gridSize; y++ {
 		for x := 0; x < gridSize; x++ {
-			inputQueue := q.NewFIFO()
-			outputQueue := q.NewFIFO()
-			finalMemory := make(chan []int64)
-
-			go intcode.ExecuteProgram(droneSystemProgram, finalMemory, inputQueue, outputQueue, 1024)
-
-			inputQueue.Enqueue(int64(x))
-			inputQueue.Enqueue(int64(y))
-
-			resultElement, _ := outputQueue.DequeueOrWaitForNextElement()
-			result := resultElement.(int64)
-
-			if result == 1 {
+			if isInBeam(x, y, droneSystemProgram) {
 				totalAffectedCells++
 				fmt.Print("#")
 			} else {
 				fmt.Print(".")
 			}
-
-			<-finalMemory
 		}
 
 		fmt.Println()
 	}
 
 	return totalAffectedCells
+}
+
+// FindClosestSquare searches for the closest square of the given size which is contained in the tractor beam.
+// Returns the top left coordinate.
+func FindClosestSquare(droneSystemProgram []int64, squareSize int) geometry.Vector {
+	x := findInitialXOfEdge(squareSize*4, droneSystemProgram)
+
+	for y := squareSize * 4; ; y++ {
+		if !isInBeam(x, y, droneSystemProgram) {
+			x++
+
+			if !isInBeam(x, y, droneSystemProgram) {
+				panic(fmt.Errorf("%d, %d should be in the beam", x, y))
+			}
+		}
+
+		squareOffset := squareSize - 1
+
+		if isInBeam(x, y-squareOffset, droneSystemProgram) && isInBeam(x+squareOffset, y-squareOffset, droneSystemProgram) {
+			return geometry.Vector{X: x, Y: y - squareOffset}
+		}
+	}
+}
+
+func findInitialXOfEdge(y int, droneSystemProgram []int64) int {
+	for x := 0; ; x++ {
+		if isInBeam(x, y, droneSystemProgram) {
+			return x
+		}
+	}
+}
+
+func isInBeam(x, y int, droneSystemProgram []int64) bool {
+	inputQueue := q.NewFIFO()
+	outputQueue := q.NewFIFO()
+	finalMemory := make(chan []int64)
+
+	go intcode.ExecuteProgram(droneSystemProgram, finalMemory, inputQueue, outputQueue, 1024)
+
+	inputQueue.Enqueue(int64(x))
+	inputQueue.Enqueue(int64(y))
+
+	resultElement, _ := outputQueue.DequeueOrWaitForNextElement()
+	result := resultElement.(int64)
+
+	<-finalMemory
+
+	return result == 1
 }
